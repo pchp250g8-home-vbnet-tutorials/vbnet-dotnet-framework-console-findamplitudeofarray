45e21c8238da69f5c872f9156793579da45ca998
Alexander <pchp250g8@tutamail.com>
2026-01-10 16:51:13 +0300

    Добавьте файлы проекта.

---
+++ FindAmplitudeOfArray/Module1.vb
@@ -0,0 +1,39 @@
+﻿Module Module1
+
+    Sub Main()
+		Dim oRnd = New Random()
+		Console.WriteLine("Input a count of elements:")
+		Dim nElems As Integer
+		Integer.TryParse(Console.ReadLine(), nElems)
+		If nElems = 0 Then
+			Console.WriteLine("Empty arrays not allowed")
+			Console.Read()
+			Return
+		End If
+		Console.WriteLine($"Generating array by filling {nElems}:")
+		Dim iNumbers(nElems - 1) As Integer
+		For i As Integer = 0 To nElems - 1
+			iNumbers(i) = oRnd.Next(1, 100)
+			Console.Write(iNumbers(i) & " ")
+		Next i
+		Dim iMax = iNumbers(0)
+		Dim iMin = iNumbers(0)
+		Dim nMaxIndex = 0
+		Dim nMinIndex = 0
+		For i As Integer = 1 To nElems - 1
+			If iNumbers(i) > iMax Then
+				iMax = iNumbers(i)
+				nMaxIndex = i
+			End If
+			If iNumbers(i) < iMin Then
+				iMin = iNumbers(i)
+				nMinIndex = i
+			End If
+		Next i
+		Console.WriteLine(String.Format(vbCrLf & "Found maximum element:{0} with index {1}", iMax, nMaxIndex))
+		Console.WriteLine(String.Format("Found minimum element:{0} with index {1}", iMin, nMinIndex))
+		Console.WriteLine($"Found amplitude: {iMax - iMin}")
+		Console.Read()
+	End Sub
+
+End Module
---
+++ FindAmplitudeOfArray/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Общие сведения об этой сборке предоставляются следующим набором
+' набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+' связанные со сборкой.
+
+' Проверьте значения атрибутов сборки
+
+<Assembly: AssemblyTitle("FindAmplitudeOfArray")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("FindAmplitudeOfArray")>
+<Assembly: AssemblyCopyright("Copyright ©  2026")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+<Assembly: Guid("663389fc-c95c-4617-8303-4f13a3c2f3ad")>
+
+' Сведения о версии сборки состоят из следующих четырех значений:
+'
+'      Основной номер версии
+'      Дополнительный номер версии
+'   Номер сборки
+'      Редакция
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
